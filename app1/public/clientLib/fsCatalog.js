4fac1ba5f60294ec542f95915cf1f0a99117fe55
jpryan <jpryan@localhost>
2026-09-26 07:50:19 -0600

    Deploy Mathematics and the Middle Way to middlewaymath.app

--- app1/public/clientLib/fsCatalog.js
+++ app1/public/clientLib/fsCatalog.js
@@ -5,7 +5,7 @@
  * Generated automatically by nodeUtils/harvestStencils.ts
  */
 export const FS_CATALOG = {
-    "generatedAt": "2026-09-26T13:38:28.083Z",
+    "generatedAt": "2026-09-26T13:49:23.560Z",
     "version": "1.0.0",
     "formalStatements": [
         {
